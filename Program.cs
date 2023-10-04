1e14a92c03e1ada200472aaf0ae8b54f70536937
João Paulo <54214033+joaocostadev@users.noreply.github.com>
2023-10-04 20:19:15 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("123456", "Modelo 1", "1111111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Smartphone iphone = new Iphone("654321", "Modelo 2", "222222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
